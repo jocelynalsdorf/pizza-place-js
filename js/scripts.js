a1a4ce705929a9d5315363e596cff1f71028467d
joss <jfrodsla@gmail.com>
2015-06-05 09:19:03 -0700

    add basic Pizza object constructor; first 2 specs passing

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+function Pizza(toppings, size, orderName) {
+	this.toppings = toppings;
+	this.size = size;
+	this.orderName = orderName;
+}
--- spec/specs.js
+++ spec/specs.js
@@ -6,6 +6,14 @@ describe('Pizza', function() {
     expect(testPizza.orderName).to.equal("Todd");
   });
 
+  it('creates a new pizza with the given specifications', function() {
+    var testPizza = new Pizza("cheese", "large", "Todd");
+    expect(testPizza.toppings).to.equal("cheese");
+    expect(testPizza.size).to.equal("large");
+    expect(testPizza.orderName).to.equal("Todd");
+  });
+
+
 
 
 });
