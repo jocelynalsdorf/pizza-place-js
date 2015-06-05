06fe202d2bd9633504c1049f43447fd8533882cb
joss <jfrodsla@gmail.com>
2015-06-05 09:44:37 -0700

    adds price method to Pizza object and tests all combinations of prices; all specs passing

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,24 @@ function Pizza(toppings, size, orderName) {
 	this.toppings = toppings;
 	this.size = size;
 	this.orderName = orderName;
-}
+
+  this.price = function() {
+  		var totalPrice = 8;
+  	if(this.toppings === "cheese" && this.size === "large") {
+  		 totalPrice += 1;
+  	}
+  	if(this.toppings === "pepperoni" && this.size === "small") {
+  		totalPrice += 2;
+  	}
+  	if(this.toppings === "pepperoni" && this.size === "large") {
+  		 totalPrice += 3;
+  	}
+    if((this.toppings === "veggie" || this.toppings === "mushroom") && this.size === "small") {
+  		totalPrice = totalPrice + 4;
+  	}
+  	if((this.toppings === "veggie" || this.toppings === "mushroom") && this.size === "large"){
+  		totalPrice += 5;
+  	}
+  	return totalPrice;
+  	}
+  }
--- spec/specs.js
+++ spec/specs.js
@@ -1,19 +1,58 @@
 describe('Pizza', function() {
   it('creates a new pizza with the given specifications', function() {
-    var testPizza = new Pizza("pepperoni", "small", "Todd");
+    var testPizza = new Pizza("pepperoni", "small", "Michael");
     expect(testPizza.toppings).to.equal("pepperoni");
     expect(testPizza.size).to.equal("small");
-    expect(testPizza.orderName).to.equal("Todd");
+    expect(testPizza.orderName).to.equal("Michael");
   });
 
   it('creates a new pizza with the given specifications', function() {
-    var testPizza = new Pizza("cheese", "large", "Todd");
+    var testPizza = new Pizza("cheese", "large", "Adam");
     expect(testPizza.toppings).to.equal("cheese");
     expect(testPizza.size).to.equal("large");
-    expect(testPizza.orderName).to.equal("Todd");
+    expect(testPizza.orderName).to.equal("Adam");
+  });
+  it('determines the price of 1 pizza with the given specifications', function() {
+    var testPizza = new Pizza("cheese", "small", "Debbie");
+    expect(testPizza.toppings).to.equal("cheese");
+    expect(testPizza.size).to.equal("small");
+    expect(testPizza.orderName).to.equal("Debbie");
+    expect(testPizza.price()).to.equal(8);
   });
 
+  it('determines the price of 1 pizza with the given specifications', function(){
+    var testPizza = new Pizza("cheese", "large", "Mike");
+    expect(testPizza.price()).to.equal(9);
+  });
 
+  it('determines the price of 1 pizza with the given specifications', function(){
+    var testPizza = new Pizza("pepperoni", "small", "Cody");
+    expect(testPizza.price()).to.equal(10);
+  });
 
+  it('determines the price of 1 pizza with the given specifications', function(){
+    var testPizza = new Pizza("pepperoni", "large", "Jeannette");
+    expect(testPizza.price()).to.equal(11);
+  });
+
+  it('determines the price of 1 pizza with the given specifications', function() {
+    var testPizza = new Pizza("mushroom", "small", "John");
+    expect(testPizza.price()).to.equal(12);
+  });
+
+  it('determines the price of 1 pizza with the given specifications', function() {
+    var testPizza = new Pizza("mushroom", "large", "Cindy");
+    expect(testPizza.price()).to.equal(13);
+  });
+
+  it('determines the price of 1 pizza with teh given specifications', function(){
+    var testPizza = new Pizza("veggie", "small", "Joss");
+    expect(testPizza.price()).to.equal(12);
+  });
+
+  it('determines the price of 1 pizza with the given specifications', function (){
+    var testPizza = new Pizza("veggie", "large", "Steve");
+    expect(testPizza.price()).to.equal(13);
+  });
 
 });
